25e3202c251dfb6a1dbfb74d1a1a83960403aab0
Carlos Maceira <carlosmgc2003@gmail.com>
2018-08-15 18:41:24 -0300

    Se agregan vistas de detalle de registros

--- clasif_mat/views.py
+++ clasif_mat/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Registro
 
 # Create your views here.
 def lista_registros(request):
     registros = Registro.objects.order_by('fecha_digit')
     return render(request, 'clasif_mat/lista_registros.html',{'registros':registros})
+
+def detalle_registro(request,pk):
+    registros = get_object_or_404(Registro, pk=pk)
+    return render(request, 'clasif_mat/registro_detalle.html', {'registros':registros})
