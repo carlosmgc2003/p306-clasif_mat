5745ca5495ca65067b3c7a234b4dc40b5bd95c04
Carlos Maceira <carlosmgc2003@gmail.com>
2018-09-05 20:08:04 -0300

    Agregados modificaciones al modelo registro

--- clasif_mat/forms.py
+++ clasif_mat/forms.py
@@ -5,10 +5,16 @@ from django.utils.translation import ugettext_lazy as _
 class CargarFormulario(forms.ModelForm):
     class Meta:
         model = Registro
-        fields = ('anio','contenido','cant_hojas','archivo')
+        fields = ('anio','contenido','estado_conservacion',
+        'estado_escaneo','continente','cant_hojas',
+        'archivo','observaciones')
         labels = {
             'anio': _('Año:'),
             'contenido': _('Contenido:'),
+            'estado_conservacion':_('Estado de conservación:'),
+            'estado_escaneo':_('Estado de escaneo:'),
+            'continente':_('Tipo de continente:'),
             'cant_hojas': _('Páginas:'),
-            'archivo': _('Archivo:')
+            'archivo': _('Archivo:'),
+            'observaciones':_('Observaciones:')
         }
--- clasif_mat/models.py
+++ clasif_mat/models.py
@@ -6,14 +6,34 @@ for i in range(1930,1991):
     ANIOS_VALIDOS.append((i,i))
 # Create your models here.
 
+ESTADO_CONSERVACION = [(0,"Seleccionar"),
+    (1,"Malo"),
+    (2,"Regular"),
+    (3,"Bueno"),
+    (4,"Muy bueno"),
+    (5,"Excelente")]
+
+ESTADO_ESCANEO = [(0,"Seleccionar"),
+    (1,"Completo"),
+    (2,"Incompleto")]
+
+CONTINENTE = [(0,"Seleccionar"),
+    (1,"Libro"),
+    (2,"Perforado"),
+    (3,"Anillado")]
+
 class Registro(models.Model):
     autor = models.ForeignKey('auth.User', on_delete = models.CASCADE)
     anio = models.PositiveSmallIntegerField(choices = ANIOS_VALIDOS)
     contenido = models.CharField(max_length = 400, help_text = "Breve descripcion hasta 400 carácteres.")
+    estado_conservacion = models.PositiveSmallIntegerField(choices = ESTADO_CONSERVACION,default=0)
+    estado_escaneo = models.PositiveSmallIntegerField(choices = ESTADO_ESCANEO,default=0)
+    continente = models.PositiveSmallIntegerField(choices = CONTINENTE,default=0)
     cant_hojas = models.PositiveSmallIntegerField(default = 1)
     fecha_creacion = models.DateTimeField(auto_now_add = True)
     #Aca se guardará el archivo PDF(no validado)... Se concatena a lo determinado en setting.py MEDIA_ROOT
     archivo = models.FileField(upload_to = 'uploads/%Y/%m/%d/',default = '',help_text = "Archivo PDF sin espacios ni carácteres especiales en el nombre.")
+    observaciones = models.TextField(max_length=1000,null=True)
 
    
     def __str__(self):
